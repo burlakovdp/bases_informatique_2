5f009c099e51714de11fac2e2a39950596eec9d3
Danyil-Polina Burlakov <burlakov.dp@protonmail.com>
2026-01-31 13:15:00 +0100

    feat: add TP #2, ex.1 (not finished yet)

--- TP/TP #2.py
+++ TP/TP #2.py
@@ -1,1 +1,23 @@
 #Ex.1
+
+def compare_et_échange(L,i):
+    if L[i] > L[i+1]:
+        L[i], L[i+1] = L[i+1], L[i]
+    return L
+
+def passe_tri_bulles(L,k):
+    for i in range(k):
+        tmp = L
+        for j in range(len(L)-1):
+            L = compare_et_échange(L, j)
+        
+        if tmp == L:
+            return L
+            
+    return L
+
+def tri_bulles(L):
+    for i in range(len(L)-1):
+        L =  passe_tri_bulles(L,i)
+    return L
+
