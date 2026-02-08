d9010c98eb50229ec2360ff2c01908d8d6897eb2
Danyil-Polina Burlakov <burlakov.dp@protonmail.com>
2026-02-08 18:51:50 +0100

    fix: TD #2

--- TP/TP #2.py
+++ TP/TP #2.py
@@ -1,23 +1,53 @@
 #Ex.1
-
-def compare_et_échange(L,i):
+def compare_et_echanger(L, i):
     if L[i] > L[i+1]:
         L[i], L[i+1] = L[i+1], L[i]
-    return L
 
-def passe_tri_bulles(L,k):
-    for i in range(k):
-        tmp = L
-        for j in range(len(L)-1):
-            L = compare_et_échange(L, j)
-        
-        if tmp == L:
-            return L
-            
-    return L
+def passe_tri_bulles(L, k):
+    for i in range(len(L)-k-1):
+        compare_et_echanger(L, i)
 
 def tri_bulles(L):
-    for i in range(len(L)-1):
-        L =  passe_tri_bulles(L,i)
-    return L
+    for i in range(len(L)):
+        tmp = list(L)
+        passe_tri_bulles(L, i)
+        if tmp == L:
+            break
+L = [1, 2, 3]
+
+#tri_bulles(L)
+#print(L)
+
+#Ex.2
+
+
+
+def fusion(L1, L2, ac = []):
+    if len(L1) == 0:
+        ac += L2
+        return ac
+    elif len(L2) == 0:
+        ac += L1
+        return ac
+    if L1[0] < L2[0]:
+        ac.append(L1[0])
+        return fusion(L1[len(ac):], L2)
+    elif L2[0] < L1[0]:
+        ac.append(L2[0])
+        return fusion(L1, L2[len(ac):])
+
+def tri_fusion(L):
+    if len(L) ==  2:
+        return fusion(list(L[0]), list(L[1]))
+    elif len(L) % 2 != 0:
+        tri_fusion(L[:len(L)//2+1]) + tri_fusion(L[len(L)//2+1:])
+    else:
+        tri_fusion(L[:len(L)//2]) + tri_fusion(L[len(L)//2:])
+
+
+        
+L = [7, 6, 5, 4, 3, 2, 1, 0]
+L1 = [3, 2, 1, 0]
+#print(fusion([3], [2]))
+print(tri_fusion(L1))
 
