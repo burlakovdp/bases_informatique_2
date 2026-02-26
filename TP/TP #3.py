edc5ded4c827e47d101d7e6e0a01f3dd83b1f0f6
Danyil-Polina Burlakov <burlakov.dp@protonmail.com>
2026-02-26 13:31:17 +0100

    feat: TP#3 -> ex.2

--- TP/TP #3.py
+++ TP/TP #3.py
@@ -8,3 +8,30 @@ def suivant(ch):
 def découpage(ch, i):
     return (ch[:i], ch[i], ch[i+1:])
 
+#ex.2
+def suppression_espaces(ch):
+    res = ''
+    for e in ch:
+        if e != ' ':
+            res += e
+    return res
+
+def remplacement_négatif(ch):
+    res = ''
+    tmp = 0
+    signe = '(+-*/^<=>&|~'
+    if ch[0] == '-':
+        res += '_'
+        tmp = 1
+    for i in range(tmp, len(ch)):
+        if ch[i] == '-':
+            if res[-1] in signe:
+                res += '_'
+            else:
+                res += ch[i]
+        else:
+            res += ch[i]
+
+    return res
+
+#ex.3
