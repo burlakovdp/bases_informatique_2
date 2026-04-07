c64a379980c1b7906f09f590ca54e535420ac206
Danyil-Polina Burlakov <burlakov.dp@protonmail.com>
2026-04-07 17:01:48 +0200

    refactor: TP#5

--- TP/TP #5.py
+++ TP/TP #5.py
@@ -1,22 +1,39 @@
-#Ex.1
-def compare_et_echanger(L, i):
+import random as r
+#ex.1
+
+def compare_et_échange(L,i):
     if L[i] > L[i+1]:
         L[i], L[i+1] = L[i+1], L[i]
+        return True
+    return False
+#L = [3,4,5,1]
+#compare_et_échange(L,0)
+#assert(L) == [3, 4, 5, 1]
+#compare_et_échange(L,2)
+#assert(L) == [3, 4, 1, 5]
 
-def passe_tri_bulles(L, k):
-    for i in range(len(L)-k-1):
-        compare_et_echanger(L, i)
+def passe_tri_bulles(L,k):
+    flag = False
+    for i in range(len(L)-k):
+        flag = compare_et_échange(L,i) or flag
+    return flag
 
 def tri_bulles(L):
-    for i in range(len(L)):
-        tmp = list(L)
-        passe_tri_bulles(L, i)
-        if tmp == L:
+    for i in range(1, len(L)):
+        flag = passe_tri_bulles(L,i)
+        if i == 1 and not flag:
+            #print('ICI')
             break
-L = [1, 2, 3]
+            
 
-#tri_bulles(L)
-#print(L)
+for i in range(10):
+    test = []
+    for j in range(10):
+        test.append(r.randint(0, 10))
+    #print(test, end=" ")
+    tri_bulles(test)
+    #print(test)
+    assert(test) == sorted(test)    
 
 #Ex.2
 
@@ -25,69 +42,81 @@ def fusion(L1, L2):
         return L2
     elif len(L2) == 0:
         return L1
+    elif L1[0] < L2[0]:
+        return [L1[0]] + fusion(L1[1:], L2)
     else:
-        if L1[0] > L2[0]:
-            return L2[:1] + fusion(L1, L2[1:])
-        return L1[:1] + fusion(L1[1:], L2) 
-        
+        return [L2[0]] + fusion(L1, L2[1:])
+
 def tri_fusion(L):
     if len(L) < 2:
         return L
     else:
         L1 = tri_fusion(L[:len(L)//2])
         L2 = tri_fusion(L[len(L)//2:])
+        #print(L1, L2)
         return fusion(L1, L2)
-    
-assert(tri_fusion([])) == []
-assert(tri_fusion([1, 2, 3, 4, 5])) == [1, 2, 3, 4, 5]
-assert(tri_fusion([5, 4, 3, 2, 1])) == [1, 2, 3, 4, 5]
-assert(tri_fusion([4, 5, 2, 1, -1, 0, 20])) == [-1, 0, 1, 2, 4, 5, 20]
+        
+
+for i in range(100):
+    test = []
+    for j in range(10):
+        test.append(r.randint(0, 10))
+    #print(test, end=" ")
+    test = tri_fusion(test)
+    #print(test)
+    assert(test) == sorted(test) 
+
 
 #Ex.3
 
-def max_list(L):
-    tmp = -1
-    for i in range(len(L)):
-        if L[i] > tmp:
-            tmp = L[i]
-    return tmp
+#print(tri_fusion([5, 1, 3, 4, 2]))
 
 def liste_nombre_occurrences(L):
-    res = []
-    for i in range(max_list(L)+1):
-        counter = 0
-        for j in range(len(L)):
-            if i == L[j]:
-                counter += 1
-        res.append(counter)
+    res = [0]*(max(L)+1)
+    for i in range(len(L)):
+        res[L[i]] += 1
     return res
 
-def cree_liste_triee(N):
-    res = []
-    for i in range(len(N)):
-        for j in range(N[i]):
-            res.append(i)
+def crée_liste_triée(N):
+    res = [i for i in range(len(N)) for j in range(N[i])]
     return res
 
 def tri_comptage(L):
-    return cree_liste_triee(liste_nombre_occurrences(L))
+    return crée_liste_triée(liste_nombre_occurrences(L))
 
+for i in range(10):
+    test = []
+    for j in range(100):
+        test.append(r.randint(0, 100000))
+    #print(test, end=" ")
+    assert(tri_comptage(test)) == sorted(test)  
 
 #Ex.4
 
 def tri_hollandais(L):
     b = 0
-    r = len(L)-1
     i = 0
-    while True:
-        if i > r:
-            return
+    r = len(L)
+    while i < r:
         if L[i] == 'B':
-            L[b], L[i] = L[i], L[b]
+            L[i], L[b] = L[b], L[i]
             b += 1
             i += 1
         elif L[i] == 'R':
-            L[r], L[i] = L[i], L[r]
+            L[i], L[r-1] = L[r-1], L[i]
             r -= 1
         elif L[i] == 'W':
             i += 1
+"""
+test = ['B', 'W', 'W', 'B', 'R', 'W', 'W', 'R', 'W', 'B']
+tri_hollandais(test)
+print(test)
+
+
+for j in range(100):
+    test = ["BRW"[r.randint(0,2)] for i in range(10)]
+    #print(test, end=' ')
+    tri_hollandais(test)
+    if test != sorted(test):
+        print(f'{test} != {sorted(test)}')
+"""
